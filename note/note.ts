56b825cdcb06d37a4d0801cf8f8246ec4ddc301e
agusev <audetv@gmail.com>
2024-12-28 16:17:24 +0300

    added crus

--- note/note.ts
+++ note/note.ts
@@ -1,18 +1,11 @@
 namespace $ {
 
-	export class $audetv_wiki_note extends $mol_store<{
-		deatils: string
-		changed_moment: string //2021-01-01T10:00:00.000Z
-	}> {
+	export class $audetv_wiki_note extends $hyoo_crus_entity.with({
+		Body: $hyoo_crus_text
+	}) {
 
-		details( next?: string ): string {
-			return this.value( 'deatils' ) ?? ''
-		}
-
-		@ $mol_mem
-		changed_moment( next?: $mol_time_moment ) {
-			const str = this.value( 'changed_moment', next && next.toString() )
-			return str ? new $mol_time_moment( str ) : null
+		body_selection(next?: readonly [ begin: number, end: number ]) {
+			return this.Body(next)?.selection( this.land().auth().lord(), next )
 		}
 	}
 }
--- wiki.view.ts
+++ wiki.view.ts
@@ -1,22 +1,67 @@
 namespace $.$$ {
 	export class $audetv_wiki extends $.$audetv_wiki {
 
-		@ $mol_mem_key
-		Note( id: string ) {
-			return this.Store().sub( 'note=' + id, super.Note )
+		@$mol_mem_key
+		note( ref: $hyoo_crus_ref ) {
+			return this.$.$hyoo_crus_glob.Node( ref, $audetv_wiki_note )
 		}
 
-		note_id() {
-			return this.$.$mol_state_arg.value( '' ) ?? ''
+		note_current() {
+			const str = this.$.$mol_state_arg.value( '' )
+			if( !str ) return null!
+			const ref = $hyoo_crus_ref( str )
+			return this.note( ref )
 		}
 
-		Note_current() {
-			return this.Note( this.note_id() )
+		note_title( next?: string ) {
+			return this.note_current()?.title( next ) ?? ''
 		}
 
-		details( next?: string ): string {
-			// return this.$.$mol_state_local.value( 'details', next ) ?? ''
-			return this.Note_current().details( next ) ?? ''
+		note_body( next?: string ) {
+			return this.note_current()?.Body( next )?.value( next ) ?? ''
+		}
+
+		note_body_selection( next?: readonly [ begin: number, end: number ] ) {
+			return this.note_current()?.body_selection( next ) ?? [ 0, 0 ]
+		}
+
+		note_changed_moment() {
+			return this.note_current()?.last_change() ?? null!
+		}
+
+		@$mol_mem
+		add() {
+
+			const land = this.$.$hyoo_crus_glob.land_grab( $hyoo_crus_rank_orgy )
+
+			this.$.$mol_dom_context.location.href = '#!=' + land.ref().description
+			this.editing( true )
+		}
+
+		@$mol_mem
+		profile_arg() {
+			return {
+				'': this.$.$hyoo_crus_glob.home().hall_by(
+					$audetv_wiki_note, $hyoo_crus_rank_public
+				)!.ref().description
+			}
+		}
+
+		@$mol_mem
+		editing( next?: boolean ) {
+			return this.$.$mol_state_history.value( 'edit', next ) ?? true
+		}
+
+		edit_close() {
+			this.editing( false )
+		}
+
+		@$mol_mem
+		pages() {
+			return [
+				this.View_page(),
+				... this.editing() ? [ this.Edit_page() ] : []
+			]
 		}
 	}
 
